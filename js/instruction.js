ac08d22e882342dd3947d42e85d4bdb2e083c264
Maxislav <lmaxim@mail.ru>
2014-08-09 09:10:56 +0300

    d

--- js/instruction.js
+++ js/instruction.js
@@ -1,39 +1,71 @@
 angular.module('phonecatAnimations', ['ngAnimate'])
 var phonecatApp = angular.module('phonecatApp', [
-	'ngRoute',
-	'navControllers',
-	'phonecatAnimations'
+    'ngRoute',
+    'navControllers',
+    'phonecatAnimations'
 ]);
 
 
-
 phonecatApp.config(['$routeProvider',
-	function($routeProvider) {
-		$routeProvider.
-			when('/gpslogger', {
-				templateUrl: 'partials/instruction/gpslogger.html',
-				controller: 'gpslogger'
-			}).
-			when('/okoavto', {
-				templateUrl: 'partials/instruction/okoavto.html',
-				controller: 'okoavto'
-			})
-			/*otherwise({
-				redirectTo: '/gpslogger'
-			});*/
-	}]);
-
+    function ($routeProvider) {
+        $routeProvider.
+            when('/home', {
+                templateUrl: 'partials/instruction/home.html',
+                controller: 'home'
+            }).
+            when('/gpslogger', {
+                templateUrl: 'partials/instruction/gpslogger.html',
+                controller: 'gpslogger'
+            }).
+            when('/okoavto', {
+                templateUrl: 'partials/instruction/okoavto.html',
+                controller: 'okoavto'
+            }).
+            otherwise({
+                redirectTo: '/home'
+            });
+    }]);
 
 
 //angular.module('gpslogger', ['ngAnimate'])
 
 var navControllers = angular.module('navControllers', [ ])
 
-
-navControllers.controller('gpslogger', function ($scope, $http, $routeParams, $rootScope ) {
-
-})
-
-navControllers.controller('okoavto', function ($scope, $http, $routeParams, $rootScope ) {
+navControllers.controller('home', function ($scope, $http, $routeParams, $rootScope) {
+   $scope.activEl('#home', $scope)
 
 })
+
+navControllers.controller('gpslogger', function ($scope, $http, $routeParams, $rootScope) {
+    $scope.activEl('#gpslogger', $scope)
+})
+
+navControllers.controller('okoavto', function ($scope, $http, $routeParams, $rootScope) {
+    $scope.activEl('#okoavto', $scope)
+})
+
+
+phonecatApp
+    .controller('nav', function ($scope) {
+        $scope.getEl = function (el) {
+            // console.log(el)
+        }
+    })
+    .directive('h', function () {
+        return function ($scope, element, attrs) {
+            $scope.els[element[0].hash] = element
+            console.log(element)
+        }
+    })
+    .run(function ($rootScope, $location) {
+        $rootScope.els = {};
+        $rootScope.activEl = function (name, s) {
+            for (var opt in s.els) {
+                if (s.els[opt][0].hash == name) {
+                    s.els[opt].addClass('select')
+                } else {
+                    s.els[opt].removeClass('select')
+                }
+            }
+        }
+    })
